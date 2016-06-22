e28c89f31ed238538784eaaf67dd8a0f7a9a822a
ankur-sisodia <ankur.sisodia@gmail.com>
2016-06-22 15:49:51 -0700

    Update Account.java

--- lab02/Account.java
+++ lab02/Account.java
@@ -76,7 +76,7 @@ public class Account {
     public void merge(Account other) {
         // TODO Put your own code here
     	this.balance = this.balance + other.getBalance();
-    	other = new Account(0);
+    	this.other.balance = 0;
     }
 public static void main(String[] args)
 {
