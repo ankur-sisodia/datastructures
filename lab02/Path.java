b38e8a5393823fcc52fdddad79a1ba00277b6374
Ankur Sisodia <asisodia@ad.corp.google.com>
2016-06-22 15:36:42 -0700

    Completed Path.java

--- lab02/Path.java
+++ lab02/Path.java
@@ -4,5 +4,21 @@
 public class Path {
 
     /** What to do, what to do... */
+	Point currPoint;
+	Point nextPoint;
+	
+	public Path (double x, double y){
+		currPoint = new Point(0,0);
+		nextPoint = new Point(x,y);
+	}
+	
+	double getCurrX() {return currPoint.getX();}
+	double getCurrY() {return currPoint.getY();}
+	double getNextX() {return nextPoint.getX();}
+	double getNextY() {return nextPoint.getY();}
+	void iterate(double dx, double dy) {
+		currPoint = nextPoint;
+		nextPoint = new Point(currPoint.getX()+dx, currPoint.getY()+dy);
+		}
 
 }
