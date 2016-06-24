ba2083b08ed2a39263d650c9257d42cf0a8f1c43
Ankur Sisodia <asisodia@ad.corp.google.com>
2016-06-24 12:33:02 -0700

    Complete Lab 3

--- lab03/SieveOfEratosthenes.java
+++ lab03/SieveOfEratosthenes.java
@@ -4,7 +4,13 @@ public class SieveOfEratosthenes {
 		if (args.length < 1) {
 			System.out.println("You need to enter an argument!");
 		}
-		int upperBound =  Integer.parseInt(args[0])-1;
+        int upperBound = 0;
+        try {
+            upperBound = Integer.parseInt(args[0]) - 1;
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+
         int counter;
 		boolean[] isNotPrime = new boolean[upperBound];
 		for (int i = 0; i < upperBound; i++) {
@@ -21,7 +27,7 @@ public class SieveOfEratosthenes {
                 }
 			}
 		}
-		for (int i = 0; i <= upperBound; i++) {
+		for (int i = 0; i < upperBound; i++) {
 			if (!isNotPrime[i]) {
 				System.out.println(i + " is a prime number.");
 			}
