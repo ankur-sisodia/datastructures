5b867d7c8bc4e11d12f00a1406000ab1f7b38a92
Ankur Sisodia <asisodia@ad.corp.google.com>
2016-06-24 11:11:23 -0700

    Completed TriangleDrawer2.java

--- lab03/TriangleDrawer2.java
+++ lab03/TriangleDrawer2.java
@@ -2,4 +2,15 @@
  * Created by asisodia on 6/23/2016.
  */
 public class TriangleDrawer2 {
+    public static void main(String[] args) {
+        int SIZE = 10;
+
+        for (int row = 0; row < SIZE; row++){
+            for (int col = 0; col <=row; col++) {
+                System.out.print('*');
+            }
+            System.out.println("");
+        }
+
+    }
 }
