38e8263f65bdc8181cf76595f899ebc6549316fc
Ankur Sisodia <asisodia@ad.corp.google.com>
2016-06-24 11:05:04 -0700

    Completed AddingMachine.java

--- lab03/AddingMachine.java
+++ lab03/AddingMachine.java
@@ -10,16 +10,26 @@ public class AddingMachine {
 		int input;
 		int last = 1;
 		int MAXIMUM_NUMBER_OF_INPUTS = 100;
+		int[] inputArray = new int[100];
+		int counter = 0;
 		while (true) {
 			input = scanner.nextInt();
+
 			if (input == 0) {
 				if (last == 0) {
 					System.out.println("total " + total);
+					for (int i =0; i<counter;i++) {
+						System.out.println(inputArray[i]);
+					}
 					return;
 				}
 				System.out.println("subtotal " + subtotal);
 				total += subtotal;
 				subtotal = 0;
+			}
+			else {
+				inputArray[counter] = input;
+				counter++;
 			}
 			subtotal += input;
 			last = input;
---
+++ lab03/TriangleDrawer2.java
@@ -0,0 +1,5 @@
+/**
+ * Created by asisodia on 6/23/2016.
+ */
+public class TriangleDrawer2 {
+}
