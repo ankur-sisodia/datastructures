07d2c0e12930d5ee1769ebd6f8a4e2673d206443
Ankur Sisodia <asisodia@ad.corp.google.com>
2016-07-01 09:07:28 -0700

    Adding Lab 07 v1

--- lab07/RuntimeQuiz.java
+++ lab07/RuntimeQuiz.java
@@ -36,8 +36,8 @@ public class RuntimeQuiz {
         return n + n * f2(n - 1) + n * n * f2(1);
     }
 
-    public static Asymptotic f3_notation = Asymptotic.BIG_O;//
-    public static Runtime f3_runtime = Runtime.LOG_N;//
+    public static Asymptotic f3_notation = Asymptotic.BIG_THETA;//
+    public static Runtime f3_runtime = Runtime.LOGLINEAR;//
     /* When f3 is first called, start will be 0 and end will be the length of the array - 1 */
     public int f3(char[] array, int start, int end) {
         if (array.length <= 1 || end <= start) return 1;
@@ -79,8 +79,8 @@ public class RuntimeQuiz {
         }
     }
 
-    public static Asymptotic f7_notation = Asymptotic.BIG_THETA;;
-    public static Runtime f7_runtime = Runtime.CUBIC;
+    public static Asymptotic f7_notation = Asymptotic.BIG_O;
+    public static Runtime f7_runtime = Runtime.LOG_N;
     public static Asymptotic f7_omega_notation = Asymptotic.BIG_OMEGA;
     public static Runtime f7_omega_runtime = Runtime.CONSTANT;
     /* When f7 is first called, start will be 0 and end will be the length of the array - 1 */
