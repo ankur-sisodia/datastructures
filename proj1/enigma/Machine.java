57125b4da151ff1f707e4aad5f34c204e299231a
Ankur Sisodia <asisodia@ad.corp.google.com>
2016-07-01 22:09:24 -0700

    Adding proj1[A

--- proj1/enigma/Machine.java
+++ proj1/enigma/Machine.java
@@ -14,8 +14,7 @@ class Machine {
      * Set my rotors to (from left to right) ROTORS.  Initially, the rotor
      * settings are all 'A'.
      */
-    Machine()
-    {
+    Machine() {
         myRotors = new Rotor[5];
         myNumRotors = 5;
     }
@@ -25,7 +24,7 @@ class Machine {
         myNumRotors = 5;
         for (int i = 0; i < 5; i++) {
             myRotors[i] = rotors[i];
-        }; //Set to A
+        } //Set to A
     }
     /** Set my rotors according to SETTING, which must be a string of four
      *  upper-case letters. The first letter refers to the leftmost
@@ -34,7 +33,7 @@ class Machine {
         char[] settingChar = setting.toCharArray();
         for (int i = 0; i < myNumRotors - 1; i++) {
             myRotors[i + 1].set(Rotor.toIndex(settingChar[i]));
-        };
+        }
         // FIXME
     }
     /** Returns the encoding/decoding of MSG, updating the state of
