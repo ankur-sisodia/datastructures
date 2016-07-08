214b290a758de335820b4c3fed27faab1a07e2b8
Ankur Sisodia <asisodia@ad.corp.google.com>
2016-07-07 21:44:53 -0700

    Adding Lab09

--- lab09/BooleanSet.java
+++ lab09/BooleanSet.java
@@ -9,33 +9,55 @@ public class BooleanSet implements SimpleSet {
 	// Initialize a set of ints from 0 to maxElement-1.
 	public BooleanSet (int maxElement) {
 	    // YOUR CODE HERE
+		contains = new boolean[maxElement];
+        for (int i = 0; i < maxElement; i++) {
+            contains[i] = false;
+        }
 	}
 	
 	// precondition: 0 <= k < maxElement.
 	// postcondition: k is in this set.
 	public void add(int k) {
 		// YOUR CODE HERE
+        if (k >= contains.length) return;
+        contains[k] = true;
 	}
 	
 	// precondition: 0 <= k < maxElement.
 	// postcondition: k is not in this set.
 	public void remove(int k) {
 		// YOUR CODE HERE
+        if (k >= contains.length) return;
+        contains[k] = false;
 	}
 	
 	// precondition: 0 <= k < maxElement
 	// Return true if k is in this set, false otherwise.
 	public boolean contains (int k) {
 		// YOUR CODE HERE
+        if (k >= contains.length) return false;
+        if (contains[k] == true) return true;
+        return false;
 	}
 	
 	// Return true if this set is empty, false otherwise.
 	public boolean isEmpty() {
 		// YOUR CODE HERE
+        if (this.size() == 0) return true;
+        return false;
+        /*for (int i = 0; i < contains.length; i++) {
+            if (contains[i] == true) return false;
+        }
+        return true;*/
 	}
 
 	// Returns the number of items in the set
 	public int size() {
 		// YOUR CODE HERE
-	}
+        int counter = 0;
+        for (int i = 0; i < contains.length; i++) {
+            if (contains[i] == true) counter++;
+        }
+        return counter;
+    }
 }
