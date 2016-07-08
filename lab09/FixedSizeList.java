9929073feff8eb7ab04d630345aa69efc420bcf3
Ankur Sisodia <asisodia@ad.corp.google.com>
2016-07-07 22:06:47 -0700

    Adding Lab09

--- lab09/FixedSizeList.java
+++ lab09/FixedSizeList.java
@@ -9,18 +9,23 @@ public class FixedSizeList implements SimpleList {
     // number of elements it can hold
     public FixedSizeList(int capacity) {
         // YOUR CODE HERE
+        values = new int[capacity];
+        count = 0;
     }
 
     // This method should return the number of items 
     // contained in values
     public int size() {
         // YOUR CODE HERE
+        return count;
     }
 
     // This method should return true if the list
     // is empty, otherwise return false
     public boolean isEmpty() {
         // YOUR CODE HERE
+        if (count == 0) return true;
+        return false;
     }
 
     // Add the argument to the list by placing it in the first
@@ -28,22 +33,38 @@ public class FixedSizeList implements SimpleList {
     // Assume that the sequence isn't full.
     public void add(int k) {
         // YOUR CODE HERE
+        values[count] = k;
+        count++;
     }
 
     // This method removes k from the list, if it is present.
     // If k appears multiple times, it removes the first occurence of k
     public void remove(int k) {
         // YOUR CODE HERE
+        for (int i = 0; i < count; i++) {
+            if (values[i] == k) {
+                for (int j = i; j < count; j++) {
+                    values[j] = values[j+1];
+                }
+                count--;
+                return;
+            }
+        }
     }
 
     // This method returns whether or not the collection contains k
     public boolean contains(int k) {
         // YOUR CODE HERE
+        for (int i = 0; i < count; i++) {
+            if (values[i] == k) return true;
+        }
+        return false;
     }
 
     // Returns the integer stored at the i-th index in the List
     public int get(int i) {
         // YOUR CODE HERE
+        return values[i];
     }
 
     // Insert k into the sequence at position i,
@@ -52,8 +73,8 @@ public class FixedSizeList implements SimpleList {
     // Assumptions: The array isn't full, i.e. count < values.length
     // Also, i is between 0 and count, inclusive.
     public void add(int i, int k) {
-        for (int k = i + 1; k <= count; k++) {
-            values[k] = values[k-1];
+        for (int j = i + 1; j <= count; j++) {
+            values[j] = values[j-1];
         }
         values[i] = k;
         count++;
@@ -64,6 +85,10 @@ public class FixedSizeList implements SimpleList {
     // note now this is different from the one-argument remove 
     public void removeIndex(int i) {
         // YOUR CODE HERE
+        for (int j = i; i < count; i++) {
+            values[j] = values[j+1];
+        }
+        count--;
     }
 
 
